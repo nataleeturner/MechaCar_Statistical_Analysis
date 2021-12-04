67779fc9d7b34c1a38d8b9d0d6c4c98babfad4f7
nataleeturner <88349443+nataleeturner@users.noreply.github.com>
2021-12-04 16:13:56 -0600

    Updated with deliverable 3 code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,6 +8,18 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 SuspensionCoil_Data <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 
-total_summary = SuspensionCoil_Data %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep')
+total_summary <- SuspensionCoil_Data %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep')
 
-lot_summary = SuspensionCoil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep')
+lot_summary <- SuspensionCoil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI), SD = sd(PSI), .groups = 'keep')
+
+t.test(SuspensionCoil_Data$PSI, mu=1500)
+
+Lot1 <- SuspensionCoil_Data %>% filter(Manufacturing_Lot=='Lot1')
+t.test(Lot1$PSI, mu=1500)
+
+Lot2 <- SuspensionCoil_Data %>% filter(Manufacturing_Lot=='Lot2')
+t.test(Lot2$PSI, mu=1500)
+
+Lot3 <- SuspensionCoil_Data %>% filter(Manufacturing_Lot=='Lot3')
+t.test(Lot3$PSI, mu=1500)
+
